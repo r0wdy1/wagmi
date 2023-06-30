4e897980f13f661384aa7d3119dd8bb4428c5135
vladimir <vladimir@blockscout.com>
2023-06-30 13:18:35 +0300

    reproducible error for signature not found

--- examples/_dev/src/components/Claim.tsx
+++ examples/_dev/src/components/Claim.tsx
@@ -1,12 +1,6 @@
-import { useAccount, useContractWrite, usePrepareContractWrite } from 'wagmi'
-
-import { wagmiContractConfig, merkleDropContractConfig } from './contracts'
+import { useContractWrite } from 'wagmi'
 import { useState } from 'react'
-import { useMerkleDropClaim, useMerkleDropWrite, usePrepareMerkleDropClaim } from '../generated'
-import { log } from 'console'
-
-
-
+import { usePrepareMerkleDropClaim } from '../generated'
 interface ClaimProps {
   address: HexString,
   claim: Claim
@@ -14,80 +8,27 @@ interface ClaimProps {
 }
 export const Claim = (props: ClaimProps) => {
 
-
-  // const { config } = usePrepareContractWrite({
-  //   ...wagmiContractConfig,
-  //   functionName: 'mint',
-  // })
-
-  // const { address, isConnecting, i sDisconnected } = useAccount();
-
   const claim = props.claim;
-  const [zkAddress, setZkAddress] = useState("");
-  const args = [BigInt(claim?.amount ?? 0), props.merkleRoot, claim.proof, zkAddress]
-  console.log('args', args)
+  const [zkAddress, setZkAddress] = useState<HexString>("0x");
 
-  const { write: writeClaim } = useMerkleDropClaim();
+  const { config: prepareClaimConfig } =
+    usePrepareMerkleDropClaim({ args: [BigInt(claim.amount), props.merkleRoot, claim.proof, zkAddress] });
 
-  // const { config: prepareClaimConfig } =
-  //   usePrepareMerkleDropClaim();
-
-  // const { write: prepareClaim, data, error, isLoading, isError, isSuccess } =
-  //   useContractWrite(prepareClaimConfig)
-  // const { config } = usePrepareContractWrite({
-  //   abi: [{
-  //     "inputs": [
-  //       {
-  //         "internalType": "uint256",
-  //         "name": "cumulativeAmount",
-  //         "type": "uint256"
-  //       },
-  //       {
-  //         "internalType": "bytes32",
-  //         "name": "expectedMerkleRoot",
-  //         "type": "bytes32"
-  //       },
-  //       {
-  //         "internalType": "bytes32[]",
-  //         "name": "merkleProof",
-  //         "type": "bytes32[]"
-  //       },
-  //       {
-  //         "internalType": "bytes",
-  //         "name": "zkAddress",
-  //         "type": "bytes"
-  //       }
-  //     ],
-  //     "name": "claim",
-  //     "outputs": [],
-  //     "stateMutability": "nonpayable",
-  //     "type": "function"
-  //   }],
-  //   address: '0xC6A4A86747aA66FC0eDd1b6033886281dc78e72e',
-  //   functionName: 'claim',
-  //   args: [BigInt(claim.amount), props.merkleRoot, claim.proof, zkAddress]
-  // })
-  // const { write, data, error, isLoading, isError, isSuccess } =
-  //   useContractWrite(config)
-
-  console.log("found proof ", claim.proof)
-  // console.log("isLoading = ", isLoading)
-
-  // writeClaim({args: [BigInt(claim.amount), props.merkleRoot, claim.proof, zkAddress]})
+  const { write: prepareClaim, data, error, isLoading, isError, isSuccess } =
+    useContractWrite(prepareClaimConfig)  
   return (
     <div>
       <div>Claim reward</div>
-      <input type="text" name="zk Address" id="zkAddress" onChange={(e) => setZkAddress(e.currentTarget.value)} />
+      <input type="text" name="zk Address" id="zkAddress" onChange={(e) => setZkAddress(`0x${e.currentTarget.value}`)} />
       <button
         type='button'
-        // disabled={isLoading || !prepareClaim?.({args:[BigInt(claim.amount), props.merkleRoot, claim.proof, zkAddress]})}
-        onClick={() => writeClaim?.({args: [BigInt(claim.amount), props.merkleRoot, claim.proof, zkAddress]})}
-        // onClick={() => {}}
+        disabled={isLoading || !prepareClaim}
+        onClick={() => prepareClaim?.()}
       >
         claim
       </button>
-      {/* {isError && <div>{error?.message}</div>} */}
-      {/* {isSuccess && <div>Transaction hash: {data?.hash}</div>} */}
+      {isError && <div>{error?.message}</div>}
+      {isSuccess && <div>Transaction hash: {data?.hash}</div>}
     </div>
   )
 }
